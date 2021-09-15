7f4b6c86d277460113563d023a97e9fd7892a396
Helene Behrens <helen@dhcp-10-22-45-105.wlan.ntnu.no>
2021-09-15 18:31:36 +0200

    Version that works with an implementation of the same model with inlabru and STAN. Synthetic data, not very realistic model

--- Master Thesis Code/Scripts/Exploration/explore_STAN_v2.R
+++ Master Thesis Code/Scripts/Exploration/explore_STAN_v2.R
@@ -13,12 +13,13 @@ set.seed(seed)
 nx = 10
 nt = 10
 
-N = 500
+#N = 500
+N = nx*nt
 
 at.risk = 1000
 
-#obs <- data.frame(expand.grid(x=1:nx, t=1:nt))
-obs = data.frame(x = sample(1:nx, N, replace = TRUE), t = sample(1:nx, N, replace = TRUE))
+obs <- data.frame(expand.grid(x=1:nx, t=1:nt))
+#obs = data.frame(x = sample(1:nx, N, replace = TRUE), t = sample(1:nx, N, replace = TRUE))
 
 #  Standard deviation of 0.1 for the iid effects (beta)
 tau.beta = 1/0.1**2
@@ -56,14 +57,14 @@ A.mat = matrix(1, nrow = 1, ncol = 10)
 e.vec = 1
 
 # common uninformative prior distribution:
-pc.prior <- list(prec = list(prior = "pc.prec", param = c(1,0.5)))
+pc.prior <- list(prec = list(prior = "pc.prec", param = c(1,0.05)))
 
 comp = ~ -1 + 
   Int(1) + 
-  alpha(x, model = "rw1", values=1:nx, constr = TRUE, hyper = pc.prior) + 
+  alpha(x, model = "rw1", values=1:nx, constr = TRUE, hyper = pc.prior, scale.model=TRUE) + 
   phi(t, model = "linear", prec.linear = 1) +
   beta(x.c, model = "iid", extraconstr = list(A = A.mat, e = e.vec), hyper = pc.prior) + 
-  kappa(t.c, model = "rw1", values = 1:nt, constr = TRUE, hyper = pc.prior) +
+  kappa(t.c, model = "rw1", values = 1:nt, constr = TRUE, hyper = pc.prior, scale.model=TRUE) +
   epsilon(xt, model = "iid", hyper = pc.prior)
 
 formula = Y ~ -1 + Int + alpha + beta*phi + beta*kappa + epsilon
@@ -87,7 +88,7 @@ palette.basis <- c('#70A4D4', '#ECC64B', '#93AD80', '#da9124', '#696B8D',
                    '#3290c1',
                    '#5d8060', '#D7B36A', '#826133', '#A85150')
 
-data.alpha = cbind(res.inlabru$summary.random$alpha, alpha.true = alpha[res.inlabru$summary.random$alpha$ID])
+data.alpha = cbind(res.inlabru$summary.random$alpha, alpha.true = alpha_true[res.inlabru$summary.random$alpha$ID])
 p.alpha <- ggplot(data = data.alpha, aes(x = ID)) + 
   geom_ribbon(aes(ymin = `0.025quant`, ymax = `0.975quant`, fill = "Estimated"), alpha = 0.4) + 
   geom_point(aes(y = alpha.true, color = "True value", fill = "True value")) + 
@@ -100,7 +101,7 @@ p.alpha <- ggplot(data = data.alpha, aes(x = ID)) +
 
 p.alpha
 
-data.beta = cbind(res.inlabru$summary.random$beta, beta.true = beta[res.inlabru$summary.random$beta$ID])
+data.beta = cbind(res.inlabru$summary.random$beta, beta.true = beta_true[res.inlabru$summary.random$beta$ID])
 p.beta <- ggplot(data = data.beta, aes(x = ID)) + 
   geom_ribbon(aes(ymin = `0.025quant`, ymax = `0.975quant`, fill = "Estimated"), alpha = 0.4) + 
   geom_point(aes(y = beta.true, color = "True value", fill = "True value")) + 
@@ -113,7 +114,7 @@ p.beta <- ggplot(data = data.beta, aes(x = ID)) +
 
 p.beta
 
-data.kappa = cbind(res.inlabru$summary.random$kappa, kappa.true = kappa[res.inlabru$summary.random$kappa$ID])
+data.kappa = cbind(res.inlabru$summary.random$kappa, kappa.true = kappa_true[res.inlabru$summary.random$kappa$ID])
 
 p.kappa <- ggplot(data = data.kappa, aes(x = ID)) + 
   geom_ribbon(aes(ymin = `0.025quant`, ymax = `0.975quant`, fill = "Estimated"), alpha = 0.4) + 
@@ -141,3 +142,101 @@ p.eta <- ggplot(data = data.eta) +
   geom_point(aes(x = eta.sim, y = true.eta), color = palette.basis[1]) + 
   labs(x="Estimated eta", y="True value for eta", title = "Eta")
 p.eta
+
+#   ----   Implement the model in STAN   ---- 
+
+exploration_data <- list(
+  X=nx,
+  T=nt,
+  x=data$x,
+  t=data$t,
+  E = data$E,
+  Y = data$Y
+)
+
+fit <- stan(
+  file="explore_STAN_v2.stan",
+  data = exploration_data,
+  chains=4,
+  warmup = 1000,
+  iter = 10000,
+  refresh = 500,
+  seed=123
+)
+
+#   ----   View results from model fitting using STAN   ----
+pairs(fit, pars=c("alpha[1]", "beta[1]", "kappa[1]", "phi", "eta[1]"), condition = "accept_stat__")
+
+print(fit)
+plot(fit)
+fit_df <- as.data.frame(fit)
+fit_summary <- summary(fit)
+print(fit_summary$summary)
+fir_summary_df <- as.data.frame(fit_summary$summary)
+
+# look at alpha
+summary_alpha <- fir_summary_df %>%
+  rownames_to_column("parameter") %>%
+  filter(grepl('alpha', parameter)) %>%
+  filter(!grepl('sigma_alpha', parameter)) %>%
+  filter(!grepl('alpha_raw', parameter)) %>%
+  mutate(index = parse_number(parameter)) %>%
+  mutate(true_alpha = alpha_true[index])
+
+plot_alpa <- ggplot(data=summary_alpha) +
+  geom_point(aes(x=index, y=mean, color="estimated")) + 
+  geom_line(aes(x=index, y=`2.5%`, color="estimated")) + 
+  geom_line(aes(x=index, y=`97.5%`, color="estimated")) +
+  geom_point(aes(x=index, y=true_alpha, color="true value")) +
+  ggtitle("Alpha")
+plot_alpa  
+
+# look at beta
+summary_beta <- fir_summary_df %>%
+  rownames_to_column("parameter") %>%
+  filter(grepl('beta', parameter)) %>%
+  filter(!grepl('sigma_beta', parameter)) %>%
+  mutate(index = parse_number(parameter)) %>%
+  mutate(true_beta = beta_true[index])
+
+plot_beta <- ggplot(data=summary_beta) +
+  geom_point(aes(x=index, y=mean, color="estimated")) + 
+  geom_line(aes(x=index, y=`2.5%`, color="estimated")) + 
+  geom_line(aes(x=index, y=`97.5%`, color="estimated")) +
+  geom_point(aes(x=index, y=true_beta, color="true value")) +
+  ggtitle("Beta")
+plot_beta
+
+# look at kappa
+summary_kappa <- fir_summary_df %>%
+  rownames_to_column("parameter") %>%
+  filter(grepl('kappa', parameter)) %>%
+  filter(!grepl('sigma_kappa', parameter)) %>%
+  mutate(index = parse_number(parameter)) %>%
+  mutate(true_kappa = kappa_true[index]) %>%
+  mutate(kappa_drifted = true_kappa + phi_true*index)
+
+plot_kappa <- ggplot(data=summary_kappa) +
+  geom_point(aes(x=index, y=mean, color="estimated")) + 
+  geom_line(aes(x=index, y=`2.5%`, color="estimated")) + 
+  geom_line(aes(x=index, y=`97.5%`, color="estimated")) +
+  geom_point(aes(x=index, y=true_kappa, color="true value")) +
+  geom_point(aes(x=index, y=kappa_drifted, color="true with drift"))+
+  ggtitle("Kappa")
+plot_kappa
+
+# look at eta
+summary_eta <- fir_summary_df %>%
+  rownames_to_column("parameter") %>%
+  filter(grepl('eta', parameter)) %>%
+  filter(!grepl('beta', parameter)) %>%
+  mutate(index = parse_number(parameter)) %>%
+  mutate(true_eta = data$eta)
+
+plot_eta <- ggplot(data=summary_eta) +
+  geom_point(aes(x=index, y=mean, color="estimated")) + 
+  geom_line(aes(x=index, y=`2.5%`, color="estimated")) + 
+  geom_line(aes(x=index, y=`97.5%`, color="estimated")) +
+  geom_point(aes(x=index, y=true_eta, color="true value")) +
+  ggtitle("Eta")
+plot_eta
