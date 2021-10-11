1dbbcee8666e925fd3def51f2936f5019ff9234d
Helene Behrens <helen@dhcp-10-22-46-253.wlan.ntnu.no>
2021-10-11 15:35:03 +0200

    Add more libraries to the installation file

--- Master Thesis Code/Scripts/installation.R
+++ Master Thesis Code/Scripts/installation.R
@@ -1,5 +1,15 @@
 # script for installation of libraries
 .libPaths("~/Documents/R_libraries")
 
-install.packages("ggplot2")
+#install.packages("ggplot2")
+install.packages("tidyverse")
 
+#  STAN
+Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1) # only necessary for Linux without the nodejs library / headers
+install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
+
+# inla
+install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+
+# inlabru
+install.packages("inlabru")
