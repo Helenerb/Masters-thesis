7e1051ccf18c9975779d72654e2e32eb43abe52c
Helene Behrens <helen@macbook-air.home>
2021-10-13 21:11:45 +0200

    Markov

--- Master Thesis Code/Scripts/Synthetic data/Stan analyses/v18/stan_v18.R
+++ Master Thesis Code/Scripts/Synthetic data/Stan analyses/v18/stan_v18.R
@@ -1,4 +1,4 @@
-# Running configuraton v17 with stan on Markov
+# Running configuraton v18 with stan on Markov
 
 # base script containing functions running STAN analysis
 
@@ -14,17 +14,17 @@ set_workspace <- function(config, markov=TRUE){
   return(output.path)
 }
 
-run_stan_v17 <- function(stan_program, chains= 4, warmup = 1000, iter = 10000, markov = TRUE){
+run_stan_v18 <- function(stan_program, chains= 4, warmup = 1000, iter = 10000, markov = TRUE){
   source("run_stan_functions.R")
-  output.path <- set_workspace("v17", markov)
+  output.path <- set_workspace("v18", markov)
   
   source("configurations_synthetic_data.R")
   
-  data = configuration.v17()
+  data = configuration.v18()
   
   stan_fit <- run_stan_program_lcc(data, chains=chains, warmup=warmup, iter=iter, stan_program=stan_program)
   
-  store_stan_results(fit=stan_fit, output.path=output.path, config="v17")
+  store_stan_results(fit=stan_fit, output.path=output.path, config="v18")
 }
 
-run_stan_v17("stan_analysis_lcc_phi_kappa.stan", chains=2, warmup = 50, iter = 100, markov=TRUE)
+run_stan_v18("stan_analysis_lcc_phi_kappa.stan", chains=2, warmup = 50, iter = 100, markov=TRUE)
