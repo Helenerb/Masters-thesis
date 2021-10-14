dcd1989e6bda9b82e6a496ce999ce5dc0eba3bc1
Helene Behrens <helen@dhcp-10-22-46-141.wlan.ntnu.no>
2021-10-14 11:39:48 +0200

    Add test script for drifted implementation of stan with cohort

--- Master Thesis Code/Scripts/Synthetic data/Stan analyses/v17d/stan_v17_drift.R
+++ Master Thesis Code/Scripts/Synthetic data/Stan analyses/v17d/stan_v17_drift.R
@@ -1,3 +1,6 @@
+# running the v17 configuration with the stan program where
+# the period effect is modelled as a random walk with drift. 
+
 # Running configuraton v17 with stan on Markov
 
 # base script containing functions running STAN analysis
@@ -14,8 +17,8 @@ set_workspace <- function(config, markov=TRUE){
   return(output.path)
 }
 
-run_stan_v17 <- function(stan_program, chains= 4, warmup = 1000, iter = 10000, markov = TRUE){
-  output.path <- set_workspace("v17", markov)
+run_stan_v17d <- function(stan_program, chains=4, warmup=1000, iter=10000, markov=TRUE){
+  output.path <- set_workspace(config="v17d", markov)
   source("run_stan_functions.R")
   
   source("configurations_synthetic_data.R")
@@ -24,7 +27,7 @@ run_stan_v17 <- function(stan_program, chains= 4, warmup = 1000, iter = 10000, m
   
   stan_fit <- run_stan_program_lcc(data, chains=chains, warmup=warmup, iter=iter, stan_program=stan_program)
   
-  store_stan_results(fit=stan_fit, output.path=output.path, config="v17")
+  store_stan_results(fit=stan_fit, output.path=output.path, config="v17d")
 }
 
-run_stan_v17("stan_analysis_lcc_phi_kappa.stan", chains=4, warmup = 1000, iter = 10000, markov=TRUE)
+run_stan_v17d(stan_program="stan_analysis_lcc_rw_drift.stan", chains=2, warmup = 100, iter = 300, markov=TRUE)
