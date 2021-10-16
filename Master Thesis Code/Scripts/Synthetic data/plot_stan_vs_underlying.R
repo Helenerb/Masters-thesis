e443d32e1191af6f1c8c3f00f9f7dd2c3ebce772
Helene Behrens <helen@macbook-air.home>
2021-10-16 16:56:01 +0200

    Update rw stan drifted plots

--- Master Thesis Code/Scripts/Synthetic data/plot_stan_vs_underlying.R
+++ Master Thesis Code/Scripts/Synthetic data/plot_stan_vs_underlying.R
@@ -154,6 +154,92 @@ plot.stan.vs.underlying.cohort.undrifted <- function(stan_df, obs, underlying.ef
   
 }
 
+plot.stan.vs.underlying.cohort.drifted <- function(stan_df, obs, underlying.effects, summaries){
+  
+  plot_intercept <- ggplot(data=stan_lc_df) +
+    geom_vline(aes(xintercept = mean[7], color = "estimated")) + 
+    geom_vline(aes(xintercept = `2.5%`[7], color = "estimated"), alpha = 0.5) +
+    geom_vline(aes(xintercept = `97.5%`[7], color = "estimated"), alpha = 0.5) + 
+    geom_vline(aes(xintercept = underlying.effects$age.intercept.true, color = "true")) +
+    scale_color_manual(name = " ", values = palette) + 
+    scale_fill_manual(name = " ", values = palette) + 
+    labs(title="Stan - intercept", x = "Value of intercept", y = " ")
+  
+  plot_phi <- ggplot(data=stan_lc_df) +
+    geom_vline(aes(xintercept = mean[6], color = "estimated")) + 
+    geom_vline(aes(xintercept = `2.5%`[6], color = "estimated"), alpha = 0.5) +
+    geom_vline(aes(xintercept = `97.5%`[6], color = "estimated"), alpha = 0.5) + 
+    geom_vline(aes(xintercept = underlying.effects$phi.true, color = "true")) +
+    scale_color_manual(name = " ", values = palette) + 
+    scale_fill_manual(name = " ", values = palette) + 
+    labs(title="Stan - phi", x = "Value of phi", y = " ")
+  
+  summary_alpha <- summaries$summary_alpha
+  
+  plot_alpha <- ggplot(data=summary_alpha) +
+    geom_point(aes(x=index, y=mean, color="estimated")) + 
+    geom_line(aes(x=index, y=`2.5%`, color="estimated"), alpha=0.5) + 
+    geom_line(aes(x=index, y=`97.5%`, color="estimated"), alpha=0.5) +
+    geom_point(aes(x=index, y=true_alpha, color="true")) +
+    scale_color_manual(name = " ", values = palette) + 
+    scale_fill_manual(name = " ", values = palette) + 
+    labs(title="Stan - alpha", x = "x", y = " ")
+  
+  summary_beta <- summaries$summary_beta
+  
+  plot_beta <- ggplot(data=summary_beta) +
+    geom_point(aes(x=index, y=mean, color="estimated")) + 
+    geom_line(aes(x=index, y=`2.5%`, color="estimated"), alpha=0.5) + 
+    geom_line(aes(x=index, y=`97.5%`, color="estimated"), alpha = 0.5) +
+    geom_point(aes(x=index, y=true_beta, color="true")) +
+    scale_color_manual(name = " ", values = palette) + 
+    scale_fill_manual(name = " ", values = palette) + 
+    labs(title="Stan - beta", x = "x", y = " ")
+  
+  summary_kappa <- summaries$summary_kappa
+  
+  plot_kappa <- ggplot(data=summary_kappa) +
+    geom_point(aes(x=index, y=mean, color="estimated")) + 
+    geom_line(aes(x=index, y=`2.5%`, color="estimated"), alpha=0.5) + 
+    geom_line(aes(x=index, y=`97.5%`, color="estimated"), alpha=0.5) +
+    geom_point(aes(x=index, y=kappa_drifted, color="true")) +
+    scale_color_manual(name = " ", values = palette) + 
+    scale_fill_manual(name = " ", values = palette) + 
+    labs(title="Stan - kappa", x = "t", y = " ")
+  
+  summary_gamma <- summaries$summary_gamma
+  
+  plot_gamma <- ggplot(data=summary_gamma) +
+    geom_point(aes(x=index, y=mean, color="estimated")) + 
+    geom_line(aes(x=index, y=`2.5%`, color="estimated"), alpha=0.5) + 
+    geom_line(aes(x=index, y=`97.5%`, color="estimated"), alpha=0.5) +
+    geom_point(aes(x=index, y=true_gamma, color="true")) +
+    scale_color_manual(name = " ", values = palette) + 
+    scale_fill_manual(name = " ", values = palette) + 
+    labs(title="Stan - gamma", x = "c", y = " ")
+  
+  summary_eta <- summaries$summary_eta
+  
+  plot_eta <- ggplot(data=summary_eta) +
+    geom_line(aes(x=index, y=mean, color="estimated")) + 
+    geom_line(aes(x=index, y=`2.5%`, color="estimated"), alpha=0.5) + 
+    geom_line(aes(x=index, y=`97.5%`, color="estimated"), alpha=0.5) +
+    geom_line(aes(x=index, y=true_eta, color="true"), alpha = 0.5) +
+    scale_color_manual(name = " ", values = palette) + 
+    scale_fill_manual(name = " ", values = palette) + 
+    labs(title="Stan - intercept", x = "x,t", y = " ")
+  
+  plots <- list(p.alpha = plot_alpha,
+                p.beta = plot_beta, 
+                p.kappa = plot_kappa,
+                p.gamma = plot_gamma,
+                p.intercept = plot_intercept,
+                p.eta = plot_eta,
+                p.phi = plot_phi)
+  return(plots)
+  
+}
+
 save.stan.plots.cohort.undrifted <- function(plots, path.to.storage=""){
   
   p.alpha <- plots$p.alpha
