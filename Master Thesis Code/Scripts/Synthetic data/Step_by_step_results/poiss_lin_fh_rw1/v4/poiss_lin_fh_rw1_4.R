96ab007c7a6f0ade6c70eae9f1a896dd462d29b0
Helene Behrens <helen@dhcp-10-22-44-70.wlan.ntnu.no>
2021-12-08 19:23:24 +0100

    poiss_lin_fh_rw1_4.R ready for Markov test

---
+++ Master Thesis Code/Scripts/Synthetic data/Step_by_step_results/poiss_lin_fh_rw1/v4/poiss_lin_fh_rw1_4.R
@@ -0,0 +1,218 @@
+#' This program investigates inference with:
+#' - The traditional_lc_log_prec version - Gaussian lc-model. 
+#' All random effects are modelled as iid, in inlabru and in stan
+#' Constraints are imposed as usual in inlabru, and with soft constraints in stan
+#' They hyperparameters are fixed. 
+#' 
+
+#   ----   Load libraries and set workspace   ----
+set_workspace <- function(markov=TRUE){
+  if(markov){
+    .libPaths("~/Documents/R_libraries")
+    setwd("~/Documents/GitHub/Masteroppgave/Masters-thesis/Master Thesis Code")
+  } else {
+    setwd("~/Desktop/Masteroppgave/Masters-thesis/Master Thesis Code")
+  }
+}
+
+set_workspace(markov=TRUE)
+#set_workspace(markov=FALSE)
+
+#   ----   Load libraries and set workspace   ----
+library("tidyverse")
+library("inlabru")
+library("ggplot2")
+library("INLA")
+library("patchwork")
+library("rstan")
+
+investigation.name <- "poiss_lin_fh_rw1"
+investigation.path <- file.path(investigation.name, "v4")
+
+#   ----    Retrieve the data   ----
+
+synthetic.male.lung.v4 <- function(){
+  obs <- read.csv("Data/synthetic_male_lung_4.csv")
+  
+  obs.trad <- obs %>% 
+    select(c(x, t, xt, age.int, year, x.c, alpha, beta, kappa, intercept, epsilon,
+             eta, tau.alpha, tau.beta, tau.kappa, tau.epsilon, E)) %>%
+    mutate(eta.no.error = intercept + alpha + beta*kappa) %>%
+    mutate(mr_gaussian = exp(eta)) %>%
+    mutate(Y_gaussian  = mr_gaussian * E)
+  
+  underlying.effects <- list(obs = obs.trad, nx = 18, nt = 18,
+                             alpha.true = {obs %>% filter(t == 0)}$alpha,
+                             beta.true = {obs %>% filter(t == 0)}$beta,
+                             kappa.true = {obs %>% filter(x == 0)}$kappa,
+                             intercept = unique(obs$intercept),
+                             age.intercept.true = unique(obs$intercept),
+                             tau.alpha.true = unique(obs$tau.alpha),
+                             tau.beta.true = unique(obs$tau.beta),
+                             tau.kappa.true = unique(obs$tau.kappa),
+                             tau.epsilon.true = unique(obs$tau.epsilon))
+  
+  return(list(obs = obs, underlying.effects = underlying.effects))
+}
+
+# We use this data for both inlabru and stan
+config.data <- synthetic.male.lung.v4()
+obs <- config.data$obs
+underlying.effects <- config.data$underlying.effects
+
+#   ----   Run STAN analysis   ----
+# Running traditional lc version of stan, 
+# implemented with log-precisions, random effects as iid, and no constraints
+
+stan.output  <- file.path("Scripts/Synthetic data/Step_by_step_results", investigation.path)
+source("Scripts/Synthetic\ data/run_stan_functions.R")
+
+run_stan <- function(stan_program, obs, chains, warmup, iter, output.path, config.name, markov=TRUE){
+  
+  stan_fit <- run_stan_program_lc(
+    list(obs = obs), chains=chains,warmup=warmup,
+    iter=iter, stan_program=stan_program)
+  
+  store_stan_results_pois_linear(
+    fit=stan_fit, output.path=output.path, config=config.name,
+    chains=chains, warmup=warmup, iter=iter, stan_program=stan_program)
+}
+
+run_stan(
+  stan_program="Scripts/Synthetic data/Stan analyses/stan_programs/step_by_step_results/stan_pois_lin_fh_rw1_sc.stan",
+  obs = obs, chains=4, warmup = 30, iter = 300, output.path = stan.output,
+  config.name = investigation.name, markov=F)
+
+inlabru.pois.lin.fh.rw1 <- function(obs, max_iter=30){
+  #'Implements inlabru analysis for lc model, fixing the precisions and modelling all random effects as iid
+  #'
+  #'@param obs: Contains the observed data and the real underlying random effects
+  #'@param max_iter (int): maximum number of iterations in inlabru
+  
+  nx = length(unique(obs$x))
+  nt = length(unique(obs$t))
+  
+  # constraints for the age effect beta
+  #A.beta = matrix(1, nrow = 1, ncol = nx)  
+  #e.beta = 1  
+  
+  fixed.theta.alpha <- list(prec = list(initial = log(1.96), fixed = T))
+  #fixed.theta.beta <- list(prec = list(initial = log(100), fixed = T))
+  fixed.theta.kappa <- list(prec = list(initial = log(336), fixed = T))
+  fixed.theta.epsilon <- list(prec = list(initial = log(420), fixed = T))
+  
+  comp = ~ -1 +
+    Int(1, prec.linear = 0.001, mean.linear = 0) +
+    alpha(x, model = "rw1", hyper = fixed.theta.alpha, constr = TRUE) +
+    #beta(x.c, model = "iid", hyper = fixed.theta.beta, constr = FALSE) +
+    kappa(t, model = "rw1", hyper = fixed.theta.kappa, constr = TRUE) + 
+    epsilon(xt, model = "iid", hyper = fixed.theta.epsilon, constr = FALSE)
+  
+  formula = Y ~ Int + alpha + kappa + epsilon
+  
+  likelihood = like(formula = formula, family = "poisson", data = obs, E = obs$E)
+  
+  c.compute <- list(cpo = TRUE, dic = TRUE, waic = TRUE, config = TRUE, return.marginals.predictor = TRUE)  # control.compute
+  
+  res.inlabru = bru(components = comp,
+                    likelihood, 
+                    options = list(verbose = F,
+                                   bru_verbose = 4, 
+                                   num.threads = "1:1",
+                                   control.compute = c.compute,
+                                   bru_max_iter=max_iter,
+                                   control.predictor = list(link = 1)
+                    ))
+  
+  return(res.inlabru)
+}
+
+res.inlabru <- inlabru.pois.lin.fh.rw1(obs, max_iter = 100)
+
+source("Scripts/Functions/plotters.R")
+source("Scripts/Synthetic data/plot_inlabru_vs_underlying.R")
+source("Scripts/Synthetic data/plot_inlabru_stan_compared.R")
+source("Scripts/Synthetic data/plot_stan_vs_underlying.R")
+
+output.path <- stan.output
+
+plots.summaries.inlabru <- plot.inlabru.vs.underlying.traditional.lc.fixed.effects.no.beta(
+  res.inlabru,
+  underlying.effects,
+  path.to.storage = output.path,
+  save=F)
+
+load(file.path(stan.output, paste("stan_", investigation.name, ".Rda", sep = "")))
+
+load(file=file.path(stan.output, "draws_intercept.RData"))
+load(file=file.path(stan.output, "draws_tau_epsilon.RData"))
+load(file.path(stan.output, "draws_tau_alpha.RData"))
+#load(file.path(stan.output, "draws_tau_beta.RData"))
+load(file.path(stan.output, "draws_tau_kappa.RData"))
+load(file.path(stan.output, "draws_alpha.RData"))
+#load(file.path(stan.output, "draws_beta.RData"))
+load(file.path(stan.output, "draws_kappa.RData"))
+load(file.path(stan.output, "draws_eta_100.RData"))
+load(file.path(stan.output, "draws_eta.RData"))
+load(file.path(stan.output, "draws_eta_reduced.RData"))
+load(file.path(stan.output, "draws_epsilon.RData"))
+
+stan.marginals <- list(intercept_draws = intercept_draws,
+                       tau_epsilon_draws = tau_epsilon_draws,
+                       tau_alpha_draws = tau_alpha_draws,
+                       #tau_beta_draws = tau_beta_draws,
+                       tau_kappa_draws = tau_kappa_draws,
+                       alpha_draws = alpha_draws,
+                       #beta_draws = beta_draws,
+                       kappa_draws = kappa_draws,
+                       eta_draws = eta_draws)
+
+stan.res <- produce.stan.plots(stan_df=stan_lc_df,
+                               underlying.effects=underlying.effects,
+                               plot.func=plot.stan.vs.underlying.synthetic.cancer.no.beta,
+                               save.func=save.stan.plots.lc.rw2,
+                               path.to.storage=output.path,
+                               summaries.func=produce.summaries.stan.traditional)
+
+plots_compared <- produce.compared.plots(
+  stan.summaries = stan.res$summaries,
+  stan.marginals = stan.marginals,
+  inlabru.summaries = plots.summaries.inlabru$summaries,
+  res.inlabru = res.inlabru,
+  underlying.effects = underlying.effects,
+  #plot.func = function(...) {plot.inlabru.stan.traditional.lc(..., cohort=FALSE, tau.beta.cutoff = 700, tau.kappa.cutoff = 500, tau.alpha.cutoff = 10, a45=F)},
+  #plot.func = function(...) {plot.inlabru.stan.traditional.lc.no.beta(..., cohort=FALSE, tau.beta.cutoff = 5000, tau.kappa.cutoff = 5000, tau.alpha.cutoff = 100, a45=F)},
+  plot.func = function(...) {plot.inlabru.stan.traditional.lc.fixed.hypers.no.beta(..., cohort=FALSE, tau.beta.cutoff = 5000, tau.kappa.cutoff = 5000, tau.alpha.cutoff = 100, a45=F)},
+  save.func = function(...) {save.compared.rw2(..., cohort=FALSE)},
+  path.to.storage=output.path)
+
+#   ----   Sample predictor   ----
+
+stan.predictor.df <- data.frame(eta_draws)
+plot.predictor.inlabru.stan.compared(res.inlabru, stan.predictor.df, path.to.storage = output.path, a45=T)
+
+#   ----   Plot marginals of random effects   ----
+
+#stan.beta.df <- data.frame(beta_draws)
+#plot.beta.inlabru.stan.compared(res.inlabru, stan.beta.df, path.to.storage = output.path, a45=F)
+
+stan.kappa.df <- data.frame(kappa_draws)
+plot.kappa.inlabru.stan.compared(res.inlabru, stan.kappa.df, path.to.storage = output.path)
+
+stan.epsilon.df <- data.frame(epsilon_draws)
+plot.epsilon.inlabru.stan.compared(res.inlabru, stan.epsilon.df, path.to.storage = output.path, a45 = F)
+
+#   ----   Specifically check the predictors at xt = 54:   ----
+
+pred.54.inlabru <- data.frame(res.inlabru$marginals.linear.predictor$APredictor.054)
+
+p.pred.54 <- ggplot(pred.54.inlabru) + 
+  geom_area(aes(x = x, y = y, fill = "Inlabru", color = "Inlabru"), alpha = 0.5) + 
+  geom_histogram(data = stan.predictor.df, aes(x = X54, y = after_stat(density), fill = "Stan", color = "Stan"), alpha = 0.5, bins = 100) + 
+  theme_classic() + 
+  scale_color_manual(name = "", values = palette) + 
+  scale_fill_manual(name = "", values = palette) + 
+  labs(title = "Predictor at xt=54", x = "", y = "")
+p.pred.54
+
+save.figure(p.pred.54, name = "predictor_54", path = output.path, png= F)
